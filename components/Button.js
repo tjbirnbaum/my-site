c053646c7874f2b3908bc0badc16376232c9bf41
tjbirnbaum <admin@tjbirnbaum.com>
2023-07-31 17:06:19 -0500

    More updates to the home page, plus buttons

---
+++ components/Button.js
@@ -0,0 +1,3 @@
+export default function Button({buttonClass}, {buttonText}) {
+    return <button className={buttonClass}>{buttonText}</button>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Button from '@components/Button'
 
 export default function Home() {
   return (
@@ -11,10 +12,14 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Photographer.</br>Filmmaker.</br>Professional goofball." />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Photographer. Filmmaker. Professional goofball." />
+        <p className="description">Hi there. I'm TJ: camera nerd, dad-joke enthusiast, and ardent purveyor of all things even remotely related to art.</p>
+        <p className="description">The best way to describe myself here is to simply say "I make stuff." 
+        I'm a photographer, filmmaker, and writer with a more-than-passing interest in design, art, and coding. 
+        And, instead of choosing to limit myself down to an unnessecary niche, 
+        I've chosen to build my entire business on being an all-around creative professional.</p>
+        <p className="description">So, I make stuff. What can I make for you?</p>
+        <Button className="Button1" buttonText="See my past work" />
       </main>
 
       <Footer />
