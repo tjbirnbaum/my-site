be3c0e7be5d0eb6f441931f12fe0f39cfe4acfcc
tjbirnbaum <106983381+tjbirnbaum@users.noreply.github.com>
2023-08-03 16:44:10 -0500

    style

--- components/Menu.js
+++ components/Menu.js
@@ -4,7 +4,9 @@ import Image from 'next/image';
 export default function Menu() {
     return (
         <div className={styles.menu}>
-            <Image src="C:\Users\TJ\Documents\GitHub\my-site\v2\images\TJ Birnbaum Multimedia Logo Tranparent-01 Wide.png" alt="TJ Birnbaum Multimedia" />
+            <Image
+            src="/images/TJ Birnbaum Multimedia Logo Tranparent-01 Wide.png"
+            alt="TJ Birnbaum Multimedia" />
             <ul className={styles.menuList}>
                 <li>About</li>
                 <li>Thoughts</li>
